cfd21a1a9e1160379d0e351d6284aad03677234b
David Carr <dcarr@scienceprousa.com>
2014-04-12 22:41:38 -0700

    setup triggered by checkboxes

--- app/src/main/java/com/dashcast/app/WidgetListAdapter.java
+++ app/src/main/java/com/dashcast/app/WidgetListAdapter.java
@@ -7,11 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
-import com.dashcast.app.R;
+import com.dashcast.app.activities.MainActivity;
 
+import org.brickred.socialauth.android.SocialAuthAdapter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -52,16 +54,55 @@ public class WidgetListAdapter extends BaseAdapter implements ListAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        JSONObject thisWidget = (JSONObject) getItem(i);
+        final JSONObject thisWidget = (JSONObject) getItem(i);
+        String widgetTitle = null;
+        try {
+            widgetTitle = thisWidget.getString("name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view = inflater.inflate(R.layout.widget_list_item, null);
-        TextView widgetTitle = (TextView) view.findViewById(R.id.list_widget_name);
+        TextView widgetTitleView = (TextView) view.findViewById(R.id.list_widget_name);
+        CheckBox enableCB = (CheckBox) view.findViewById(R.id.enable_checkbox);
+        enableCB.setTag(new Integer(i));
+        enableCB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "checkbox clicked");
+                CheckBox checkBox = (CheckBox)view;
+                if(checkBox.isChecked()){
+                    enableOrSetup((Integer) view.getTag());
+                }
+            }
+        });
         try {
-            widgetTitle.setText(thisWidget.getString("name"));
+            widgetTitleView.setText(widgetTitle);
             Log.d(TAG, thisWidget.getString("name"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return view;
+    }
+
+    private void enableOrSetup(Integer i) {
+        Log.d(TAG, String.valueOf(i));
+        final JSONObject thisWidget = (JSONObject) getItem(i);
+        String widgetTitle = null;
+        try {
+            widgetTitle = thisWidget.getString("name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if (widgetTitle.contains("Traffic")) {
+
+        }
+
+        if (widgetTitle.contains("Test")) {
+            SocialAuthAdapter mSocialAuthAdapter = MainActivity.getSocialAuthAdapter();
+            mSocialAuthAdapter.setTitleVisible(false);
+            mSocialAuthAdapter.authorize(mContext, SocialAuthAdapter.Provider.FACEBOOK);
+        }
     }
 }
